3c4ac030db39c3d77765e99d24d8b931d7fa7566
Makkouh2 <145025546+Makkouh2@users.noreply.github.com>
2023-10-19 16:18:16 +0200

    Ejercicio 4 otravez
    se me cambio una cosa en el ejercicio por eso lo vuelvo a subir

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,2 +1,3 @@
 x=(input("Escriba su numero de teléfono"))
 print(x[4:13])
+print(x.upper)
---
+++ Ejercicio6.py
@@ -0,0 +1,3 @@
+x=(input("Escriba una frase"))
+y=(input("Escriba una vocal"))
+print(x.replace (y,y.upper()))
